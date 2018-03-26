20816f5dd2620974de06471baff28b4ab4a2c44f
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-03-26 22:25:12 +0900

    [init]

---
+++ tracer/tracer.go
@@ -0,0 +1,32 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// Tracer is an interface representing an object which can record things happened in the code.
+type Tracer interface {
+	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(...interface{}) {}
+
+func Off() Tracer {
+	return &nilTracer{}
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
---
+++ tracer/tracer_test.go
@@ -0,0 +1,25 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("returned value from New is null")
+	} else {
+		tracer.Trace("Hello tracer package!")
+		if buf.String() != "Hello tracer package!\n" {
+			t.Errorf("'%s' is output mistakenly", buf.String())
+		}
+	}
+}
+
+func TestOff(t *testing.T) {
+	var silentTracer Tracer = Off()
+	silentTracer.Trace("data")
+	// just check if called without errors
+}
